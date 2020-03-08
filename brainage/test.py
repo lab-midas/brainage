1cbbf894c096d4bfbfc0b0dc2ac59a3e41128830
Tobias Hepp <tobias.hepp@med.uni-tuebingen.de>
2020-03-08 21:56:53 +0000

    Delete test files

--- brainage/test.py
+++
@@ -1,9 +0,0 @@
-from dotenv import load_dotenv
-load_dotenv()
-
-import matplotlib.pyplot as plt
-import os
-log_dir = os.getenv("LOG_DIR")
-model_dir = os.getenv("MODEL_DIR")
-data_dir = os.getenv("DATA_DIR")
-print(data_dir)
