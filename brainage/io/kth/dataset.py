9e4fd8da52a28f3fd1a87f021627638eec910a55
Tobias Hepp <tobias.hepp@med.uni-tuebingen.de>
2020-04-26 14:11:44 +0200

    maain

--- brainage/io/kth/dataset.py
+++ brainage/io/kth/dataset.py
@@ -1,2 +1,65 @@
+import pathlib
+import h5py
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import time
+import torch
+import logging
+import torch.nn as nn
+import torch.optim as optim
+import collections
+import copy
 from torch.utils.data import Dataset as AbstractDataset
 from torch.utils.data import DataLoader
+
+class Dataset(AbstractDataset):
+    def __init__(self,
+                 fname: pathlib.Path,
+                 ix_fname: pathlib.Path,
+                 age_data,
+                 transform=None):
+
+        super().__init__()
+
+        # copy over
+        self.fname = fname
+        self.ix_fname = ix_fname
+        self.age_data = age_data
+        self.transform = transform
+        self.logger = logging.getLogger(__name__)
+
+        # read indices
+        self.ix = sorted(set(self.ix_fname.read_text().strip().split("\n")))[:16]
+
+        self.logger.info('loading dataset to memory ...')
+        t = time.perf_counter()
+        # reading data from hdf5 to container object
+        with h5py.File(self.fname, 'r') as fhandle:
+            def load_data():
+                for example_ix in self.ix:
+                    self.logger.debug(f'loading {example_ix}')
+                    # extract data
+                    original = fhandle['original'][example_ix][:]
+                    age = float(self.age_data[example_ix])
+                    data = {'data': original[np.newaxis, np.newaxis, ...],
+                            'age': age}
+                    yield data
+            self.data_container = collections.deque(load_data())
+        self.logger.info(f'finished in {time.perf_counter() -t } seconds')
+
+    def __len__(self):
+        return len(self.ix)
+
+    def __getitem__(self, i):
+        sample = self.data_container[i]
+
+        # data augmentation
+        if self.transform:
+            sample = self.transform(**sample)
+
+        # add additional dimension
+        age = np.expand_dims(np.array(sample['age']), axis=0)
+        # squeeze batch dimension
+        data = np.squeeze(sample['data'] ,axis=0)
+        return torch.tensor(data), torch.tensor(age)
--- brainage/misc/utils.py
+++ brainage/misc/utils.py
@@ -25,37 +25,3 @@ def init_gpu(gpu_device):
             # Memory growth must be set before GPUs have been initialized
             print(e)
 
-
-def add_dataset_config(config,
-                       dataset='IXI',
-                       split='0',
-                       ADNI_test_selection='split',
-                       ADNI_group='ADNL'):
-    
-    data_dir = os.getenv("DATA_DIR")
-
-    if dataset == 'IXI':
-        # IXI
-        config['base_folder'] = os.path.join(data_dir, 'IXI/IXI_T1/PP_IXIT1')
-        config['file_prefix'] = 'fcmnorm_brain_mni_IXI'
-        config['file_suffix'] = '_T1_restore'
-        config['file_ext'] = '.nii.gz'
-        config[
-            'path_training_csv'] =os.path.join(data_dir, f'IXI/IXI_T1/config/IXI_T1_train_split{split}.csv')
-        config[
-            'path_validation_csv'] = os.path.join(data_dir,f'IXI/IXI_T1/config/IXI_T1_val_split{split}.csv')
-        config['path_test_csv'] = os.path.join(data_dir,'IXI/IXI_T1/config/IXI_T1_test.csv')
-        config['path_info_csv'] = os.path.join(data_dir,'IXI/IXI_T1/config/IXI_T1_age.csv')
-
-    if dataset == 'ADNI':
-        # ADNI
-        config['base_folder'] = os.path.join(data_dir, 'ADNI/ADNI_T1')
-        config['file_prefix'] = 'fcmnorm_brain_mni_ADNI_'
-        config['file_suffix'] = '_T1'
-        config['file_ext'] = '.nii.gz'
-        config['path_training_csv'] = os.path.join(data_dir, f'ADNI/config/GR_{ADNI_group}/{ADNI_group}_ADNI_T1_train_split{split}.csv')
-        config['path_validation_csv'] = os.path.join(data_dir, f'ADNI/config/GR_{ADNI_group}/{ADNI_group}_ADNI_T1_val_split{split}.csv')
-        config['path_test_csv'] = os.path.join(data_dir, f'ADNI/config/GR_{ADNI_group}/{ADNI_group}_ADNI_T1_test_{ADNI_test_selection}.csv')
-        config['path_info_csv'] = os.path.join(data_dir, 'ADNI/config/ADNI_age_only.csv')
-
-    return config
